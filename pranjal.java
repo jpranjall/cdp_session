bbbc2da7d8e45f823d5e51a6f5e73525c0ed6554
Pranjal Jagtap <115860841+jpranjall@users.noreply.github.com>
2023-04-24 05:29:16 +0000

    my1stcode

---
+++ pranjal.java
@@ -0,0 +1,69 @@
+
+public class pranjal {
+
+	public static void main(String[] args) 
+	{
+    int a=10;
+
+    if(a%2==0) {
+        if(a%6==0) {
+            System.out.println("even and divisible by 6");
+        }else {
+             System.out.println("even and divisible by 6");
+
+        }    
+        
+        }
+        else{
+             System.out.println("odd number");
+        }
+    }
+
+	// comment 
+
+	/*{
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=30;
+		if(b>a) {
+			int g=b-a;
+			System.out.println(g);
+		}else if(a>b){
+		  int h=a+b;
+		  System.out.println(h);
+		}else {
+			System.out.println("both are same");
+		  
+		  
+		}
+
+	}*/
+	
+	/*{
+		int a=10;
+		int b=20;
+		int c=30;
+		if(a<b&&a<c)
+		{
+			System.out.println("a is smallest number");
+		}
+		else if(a>b&&a>c)
+		{
+			System.out.println("a is largest number");
+		}
+		else
+		{
+			System.out.println("we cant decide");
+			
+		}
+		
+		
+	}*/
+}
+	
+
+
+
+
+
